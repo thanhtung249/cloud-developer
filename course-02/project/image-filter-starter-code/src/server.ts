65b05a9efc8c8d22874bda29fe3c10aa9f49a974
thanhtung249 <thanhtung249@gmail.com>
2023-06-12 22:41:18 +0700

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,7 +27,42 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+function validateURL(pURL: string) {
+    var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+    var url = new RegExp(regexQuery,"i");
+    return url.test(pURL);
+  }
+  
+  app.get( "/filteredimage", async ( req, res ) => {
+      // 1. validate the image_url query
+      var imageUrl = req.query.image_url;
+      var isImageUrlValid = validateURL(imageUrl);
+  
+      if(isImageUrlValid){
+        // 2. call filterImageFromURL(image_url) to filter the image
+        var imagePath = await filterImageFromURL(imageUrl);
+  
+        var options = {
+          dotfiles: 'deny',
+          headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+          }
+        };
+        // 3. send the resulting file in the response
+        res.sendFile(imagePath, options, function (err) {
+          if (err) {
+            res.status(400).send('The Image could not be accessed')
+          } else {
+            // 4. deletes any files on the server on finish of the response
+            deleteLocalFiles([imagePath]);
+          }
+        });
+      }
+      else {
+        res.status(404).send('The URL for the image was not found')
+      }
+  });  
   //! END @TODO1
   
   // Root Endpoint
