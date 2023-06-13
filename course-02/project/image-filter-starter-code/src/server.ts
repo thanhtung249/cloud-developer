42b219d9678daea0ee0e78080f8ed834475970ef
thanhtung249 <thanhtung249@gmail.com>
2023-06-13 22:42:49 +0700

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,6 +17,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
+  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -27,20 +28,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-function validateURL(pURL: string) {
+  function validateURL(pURL: string) {
     var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
     var url = new RegExp(regexQuery,"i");
     return url.test(pURL);
   }
   
   app.get( "/filteredimage", async ( req, res ) => {
-      // 1. validate the image_url query
-      var imageUrl = req.query.image_url;
-      var isImageUrlValid = validateURL(imageUrl);
   
-      if(isImageUrlValid){
+      // 1. validate the image_url query
+  
+      var image_url = req.query.image_url;
+      var is_image_url_valid = validateURL(image_url);
+  
+      if(is_image_url_valid){
         // 2. call filterImageFromURL(image_url) to filter the image
-        var imagePath = await filterImageFromURL(imageUrl);
+        var image_path = await filterImageFromURL(image_url);
   
         var options = {
           dotfiles: 'deny',
@@ -50,17 +53,18 @@ function validateURL(pURL: string) {
           }
         };
         // 3. send the resulting file in the response
-        res.sendFile(imagePath, options, function (err) {
+        res.sendFile(image_path, options, function (err) {
           if (err) {
-            res.status(400).send('The Image could not be accessed')
+            res.status(400).send('Image could not be accessed')
           } else {
             // 4. deletes any files on the server on finish of the response
-            deleteLocalFiles([imagePath]);
+            deleteLocalFiles([image_path]);
           }
         });
+  
       }
       else {
-        res.status(404).send('The URL for the image was not found')
+        res.status(404).send('URL for the image was not found')
       }
   });  
   //! END @TODO1
